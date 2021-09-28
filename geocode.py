ac7b7c39812d235e828f6275968b600c07dcd251
Cody Clark <opuseleven@protonmail.com>
2021-09-28 14:49:30 -0500

    Fixed a bunch of errors

--- geocode.py
+++ geocode.py
@@ -21,39 +21,48 @@ headers = {
   "apikey": os.getenv('apikey')
 }
 
-def geocode(address,city,state):
+def geocode(address):
+    print(address)
     params = (
-       ("text","%s, %s, %s, United States"% address, city, state),
+       ("text","%s, United States"% address),
     );
     response = requests.get('https://app.geocodeapi.io/api/v1/search', headers=headers, params=params)
     parsed_data = json.loads(response.text)
     coords = parsed_data['features'][0]['geometry']['coordinates']
     return coords
 
+print("Finding coordinates:")
 for sheet in workbook.worksheets:
     titlerow = sheet[1]
     state = sheet.title
-    addresscol = null
-    coordinatecol = null
+    print("State: %s"% state)
+    addresscol = -1
+    coordinatecol = -1
+    count = 0
     for cell in titlerow:
-        if cell.value = 'Address':
-            addresscol = cell.column
-        if cell.value = 'Coordinates':
-            coordinatecol = cell.column
-    if addresscol = null:
+        if cell.value == 'Address':
+            addresscol = count
+        if cell.value == 'Coordinates':
+            coordinatecol = count
+    if addresscol == -1:
         break
-    if coordinatecol = null:
+    if coordinatecol == -1:
         sheet.insert_cols(8)
-        coordinatecol = sheet['H']
-        sheet['H1'] = 'Coordinates'
+        coordinatecol = 7
+        sheet['H1'].value = 'Coordinates'
     for row in sheet.iter_rows(min_row=2):
         # convert to coord
         address = row[addresscol].value
-        city = row[citycol].value
-        coordinates = geocode(address,city,state)
-        row[coordinatecol] = coordinates
+        if address == 'None':
+            coordinates = ' '
+        else:
+            coordinates = geocode(address)
+        print(coordinates)
+        row[coordinatecol].value = coordinates
 
 # write to coordinatecol
+print("Writing file...")
 split_filename = os.path.splitext(filename)
 new_filename = split_filename[0] + '-coordinates' + split_filename[1]
 workbook.save(new_filename)
+print("Done.")
